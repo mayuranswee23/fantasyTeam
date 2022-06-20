00e69142405f016902825280ebbb388068184f77
Mayuran <mayuranswee23@gmail.com>
2022-06-20 00:43:47 -0400

    obtain values in order to update players

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function showPrompts(){
             addPlayer();
             break;
         case 'Update Player':
-            console.log('Update Player');
+            updatePlayer();
             break;
         case 'Exit':
             console.log('Bye!');
@@ -162,9 +162,7 @@ function addPlayer(){
                 choices: res.map(res=> res.id + " " + res.positions)
             }
         ]). then( response =>{
-            let positionId = response.playerPosition.split(' ')[0];
-            
-    
+        let positionId = response.playerPosition.split(' ')[0];
         console.log(clubId);
         // console.log(positionId)
         console.log(response);
@@ -180,6 +178,75 @@ function addPlayer(){
         })
     })
 })
+}
+
+function updatePlayer(){
+    const sql = `SELECT 
+    players.id,
+    players.first_name, 
+    players.last_name,
+    clubs.player_name AS club_name, 
+    clubs.id AS clubID, 
+    field_positions.positions AS Position
+    FROM players
+    INNER JOIN clubs ON players.club_id = clubs.id
+    INNER JOIN field_positions ON players.positions_id = field_positions.id;`
+
+    db.query(sql, (err, res)=> {
+        if (err) throw err; 
+        const getRes = res;
+        inquirer.prompt([
+            {
+                name: 'playerUpdate', 
+                type: 'list', 
+                message: 'Which player do you want to update?',
+                choices: res.map(res=> "ID: " + res.id + " " + res.first_name + " " + res.last_name 
+                + " Club: " + res.club_name + " Position: " + res.Position)
+            }, 
+            {
+                name: 'updateField', 
+                type: 'list', 
+                message: 'What do you want to update?', 
+                choices: ['First Name', 'Last Name', 'Club', 'Position']
+            },
+        ]).then(response => {
+            const player = response.playerUpdate;
+            const ID = player.split(' ')[1];
+            const firstName = player.split(' ')[2];
+            // console.log(firstName);
+            const lastName = player.split(' ')[3];
+            // console.log(lastName);
+            const clubIDS = getRes.map(result=> result.clubID)[ID -1 || 0]; 
+            // console.log(clubIDS);
+
+            const clubNames = getRes.map(result => result.club_name)[ID-1]
+            // console.log(clubNames);
+
+            const playerPosition = getRes.map(result => result.Position)[ID-1]
+            // console.log(playerPosition);
+
+            const updates = response.updateField; 
+            switch(updates){
+                case 'First Name':
+                    updateFirstName(ID, firstName);
+                    break; 
+                case 'Last Name':
+                    updateLastName();
+                    break;
+                case 'Club':
+                    updateClub();
+                    break;
+                case 'Position':
+                    updatePosition();
+                    break;
+            }        
+        })
+    })
+}
+
+function updateFirstName(id, name){
+ console.log(name)
+ console.log(id)
 }
 
 showPrompts();
